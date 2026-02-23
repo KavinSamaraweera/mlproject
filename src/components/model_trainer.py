6e20d8e3e24a7d5db6c06af5af8ae9de8216a8f5
Kavin Samaraweera <kavinsamaraweera@gmail.com>
2026-02-24 02:16:49 +0530

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,12 +48,50 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor()
             }
 
+            params = {
+                "Random Forest": {
+                    'n_estimators' : [8,16,32,64,128,256]
+
+                },
+
+                "Decision Tree": {
+                    'criterion' : ['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
+                },
+
+                "Gradient Boosting": {
+                    'learning_rate' : [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators' : [8,16,32,64,128,256]
+                },
+
+                "K Neighbors Regressor": {
+                    'n_neighbors' : [5,7,9,11]
+                },
+
+                "XGB Regressor": {
+                    'learning_rate' : [.1, .01, .05, .001],
+                    'n_estimators' : [8, 16, 32, 64, 128, 256]
+                },
+
+                "CatBoost Regressor": {
+                    'depth' : [6,8,10],
+                    'learning_rate' : [0.01, 0.05, 0.1],
+                    'iterations' : [30,50,100]
+                },
+
+                "AdaBoost Regressor": {
+                    'learning_rate' : [.1, .01, 0.5, .001],
+                    'n_estimators' : [8, 16, 32, 64, 128, 256]
+                }
+            }
+
             model_report: dict = evaluate_models(
                 x_train=x_train,
                 y_train=y_train,
                 x_test=x_test,
                 y_test=y_test,
-                models=models
+                models=models,
+                params = params
             )
 
             # Get best model score
